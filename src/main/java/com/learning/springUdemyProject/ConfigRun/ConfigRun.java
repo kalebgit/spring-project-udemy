b9e3de98ff4c7d8e0fd8e9baffcfc8b6e3c8b9f8
Jimenez Rivera Emiliano Kaleb <emilianokaleb@gmail.com>
2023-11-23 18:03:32 -0600

    feat(): new components added with @Component annotation

--- src/main/java/com/learning/springUdemyProject/ConfigRun/ConfigRun.java
+++ src/main/java/com/learning/springUdemyProject/ConfigRun/ConfigRun.java
@@ -2,6 +2,7 @@ package com.learning.springUdemyProject.ConfigRun;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import game.GameConsole;
@@ -9,19 +10,23 @@ import game.PacmanGame;
 
 
 @Configuration
+@ComponentScan("game")
 public class ConfigRun {
 	
-	@Bean
-	public GameConsole game() {
-		GameConsole game = new PacmanGame();
-		return game;
-	}
+	
 	
 	
 	
 	public static void main(String[] args) {
-		try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigRun.class)){\
-			
+		try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigRun.class)){
+			Object game = context.getBean("PacmanGame");
+			if(game instanceof GameConsole) {
+				GameConsole realGame = (GameConsole)game;
+				realGame.up();
+				realGame.down();
+				realGame.right();
+				realGame.left();
+			}
 		}
 	}
 }
